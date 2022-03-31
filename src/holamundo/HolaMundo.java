66dfdf69715fb21674a8306f74d05e06dc5c8934
bryanp2022 <jarquinernesto0802@gmail.com>
2022-03-31 12:08:06 -0600

    Se sube Proyecto Hola Mundo

---
+++ src/holamundo/HolaMundo.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundo;
+
+import java.util.Scanner;
+/**
+ *
+ * @author Tania Reyes
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Hola Mundo java");
+    }
+    
+}
